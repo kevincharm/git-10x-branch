47b5af18fb56cec35601c3aa81567c54188cd710
Kevin Tjiam <kevin@kevincharm.com>
2018-03-25 13:11:50 +0800

    move arg parsing logic to main function

--- src/bin.ts
+++ src/bin.ts
@@ -1,13 +1,3 @@
 #!/usr/bin/env node
-import { spawn } from 'child_process'
 import main from './index'
-
-const args = process.argv.slice(2)
-if (!args.length) {
-    main()
-} else {
-    spawn('git', ['branch', ...args], {
-        detached: true,
-        stdio: 'inherit'
-    }).unref()
-}
+main()
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,15 @@
+import { spawn } from 'child_process'
 import branchSelectionScreen from './screens/branch-selection'
 
 export default async function main() {
+    const args = process.argv.slice(2)
+    if (args.length) {
+        spawn('git', ['branch', ...args], {
+            detached: true,
+            stdio: 'inherit'
+        }).unref()
+        return
+    }
+
     await branchSelectionScreen()
 }
