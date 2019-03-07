957aee0b77a30988fd5fe668c075160ff8fcb8e4
kevincharm <kevin@kevincharm.com>
2019-03-08 00:57:12 +0800

    set env.TERM to 'windows-ansi' if platform is windows

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,10 @@ import { spawnSync } from 'child_process'
 import branchSelectionScreen from './screens/branch-selection'
 
 export default async function main() {
+    if (process.platform === 'win32') {
+        process.env.TERM = 'windows-ansi'
+    }
+
     const args = process.argv.slice(2)
     if (args.length) {
         spawnSync('git', ['branch', ...args], {
